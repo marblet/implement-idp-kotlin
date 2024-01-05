a42a5d8e2f5ecdfc4eaaa64a7a355af7e3d45160
Hibiki Taguchi <guitarmarble0117@gmail.com>
2024-01-05 10:07:57 +0900

    fix entity definition, create get-client method

--- src/main/kotlin/com/marblet/idp/infrastructure/ClientRepositoryImpl.kt
+++ src/main/kotlin/com/marblet/idp/infrastructure/ClientRepositoryImpl.kt
@@ -1,13 +1,32 @@
 package com.marblet.idp.infrastructure
 
+import com.marblet.idp.domain.model.Client
+import com.marblet.idp.domain.model.ClientId
+import com.marblet.idp.domain.repository.ClientRepository
 import org.jetbrains.exposed.sql.Table
+import org.jetbrains.exposed.sql.select
+import org.springframework.stereotype.Repository
+import org.springframework.transaction.annotation.Transactional
 
-class ClientRepository {
+@Repository
+@Transactional
+class ClientRepositoryImpl : ClientRepository {
+    override fun get(clientId: ClientId): Client? {
+        return Clients.select {
+            Clients.id eq clientId.value
+        }.firstOrNull()?.let {
+            Client(
+                clientId = ClientId(it[Clients.id]),
+                secret = it[Clients.secret],
+                redirectUris = it[Clients.redirectUris]?.split(" "),
+            )
+        }
+    }
 }
 
-object Clients: Table() {
+object Clients : Table("clients") {
     val id = varchar("id", 128)
-    val secret = varchar("username", 128)
+    val secret = varchar("secret", 128)
     val redirectUris = text("redirect_uris").nullable()
 
     override val primaryKey = PrimaryKey(id)
