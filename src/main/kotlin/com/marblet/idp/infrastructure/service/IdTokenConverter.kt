1dd1e1b28154f7cd84e91a9fd3ed42f53afa53de
Hibiki Taguchi <guitarmarble0117@gmail.com>
2024-05-21 09:56:13 +0900

    set origin to issuer

--- src/main/kotlin/com/marblet/idp/infrastructure/service/IdTokenConverter.kt
+++ src/main/kotlin/com/marblet/idp/infrastructure/service/IdTokenConverter.kt
@@ -2,6 +2,7 @@ package com.marblet.idp.infrastructure.service
 
 import com.auth0.jwt.JWT
 import com.auth0.jwt.algorithms.Algorithm
+import com.marblet.idp.configration.AppConfig
 import com.marblet.idp.configration.RsaKeyConfig
 import com.marblet.idp.domain.model.IdTokenPayload
 import com.marblet.idp.domain.service.IdTokenConverter
@@ -12,13 +13,14 @@ import java.time.ZoneId
 @Service
 class IdTokenConverter(
     private val rsaKeyConfig: RsaKeyConfig,
+    private val appConfig: AppConfig,
 ) : IdTokenConverter {
     private val algorithm = Algorithm.RSA256(null, rsaKeyConfig.rsaPrivateKey)
 
     override fun encode(payload: IdTokenPayload): String {
         return JWT.create()
             .withKeyId(rsaKeyConfig.kid)
-            .withIssuer(payload.issuer)
+            .withIssuer(appConfig.origin)
             .withSubject(payload.userId.value)
             .withAudience(payload.clientId.value)
             .withIssuedAt(Instant.ofEpochSecond(payload.issuedAt.atZone(ZoneId.systemDefault()).toEpochSecond()))
